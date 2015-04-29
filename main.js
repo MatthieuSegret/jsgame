ca3e71b52aa758e1578f7973f4f6035b7794d64f
Matthieu Segret <matthieu.segret@gmail.com>
2015-04-29 15:36:59 +0200

    Ajout du joueur. Il peut le déplacer avec les flèches du clavier.

--- main.js
+++ main.js
@@ -8,14 +8,32 @@ window.onload = function () {
   Load.prototype = {
     preload: function() {
       this.game.load.image('background', 'images/background.png');
+      this.game.load.image('player', 'images/player.png');
     },
 
     create: function() {
       this.game.add.sprite(0, 0, 'background');
+      this.player = this.game.add.sprite(570, 400, 'player');
+
+      this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+      this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+      this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+      this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
     },
 
     update: function() {
-
+      if (this.upKey.isDown) {
+        this.player.y -= 3;
+      }
+      else if (this.downKey.isDown) {
+        this.player.y += 3;
+      }
+      if (this.leftKey.isDown) {
+        this.player.x -= 3;
+      }
+      else if (this.rightKey.isDown) {
+        this.player.x += 3;
+      }
     }
   };
 
