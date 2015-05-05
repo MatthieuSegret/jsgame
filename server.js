3c2b7734d8561b8e66bfe811676da4b38d80ee9e
Matthieu Segret <matthieu.segret@gmail.com>
2015-05-05 18:25:10 +0200

    Ajout du script server.js pour le démarrage du serveur sur Heroku

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var port = process.env.PORT || 8080;
+var app = express();
+app.use(express.static("" + (process.cwd()) + "/generated"));
+app.listen(port);
+console.log("Starting express web server in \"" + (process.cwd()) + "\" on port " + port);
