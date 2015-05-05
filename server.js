6b793b79f24c3e2f870e62e3c9ad585ce9170288
Matthieu Segret <matthieu.segret@gmail.com>
2015-05-05 18:51:44 +0200

    Revert "Push generated directory"
    This reverts commit a139b6eced68c8928d74d268c29fdb960a584eac.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var port = process.env.PORT || 8080;
 var app = express();
-app.use(express.static("" + (process.cwd()) + "/"));
+app.use(express.static("/generated"));
 app.listen(port);
-console.log("Starting express web server in \"" + (process.cwd()) + "\" on port " + port);
+console.log("Starting express web server in \"" + "/generated" + "\" on port " + port);
